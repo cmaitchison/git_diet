7c1d4b403dab37ddc515e6fc09463d99bd2d0e20
cmaitchison <cmaitchison@gmail.com>
2011-10-09 19:45:23 +1100

    Moved test-repo to spec folder

--- spec/fixture.sh
+++ spec/fixture.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-[ -x test-repo ] && rm -rf test-repo
+[ -x spec/test-repo ] && rm -rf spec/test-repo
 
 (
-  mkdir test-repo
-  cd test-repo
+  mkdir spec/test-repo
+  cd spec/test-repo
   git init --quiet
   cat ../test-repo.gz | gunzip | git fast-import --quiet
   git checkout --quiet master
